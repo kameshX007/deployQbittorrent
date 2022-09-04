7a78ed8b723a751c2369f60460e1d720659e39fd
kameshX007 <kameshbook@gmail.com>
2022-09-04 16:08:23 +0530

    #

--- deployQbittorrent.sh
+++ deployQbittorrent.sh
@@ -2,12 +2,12 @@
 #Checking whether username is passed to script
 #A docker folder will be created inside this users home directory and container persistant data will be stored there
 dockerUser=$1;
-[ -z "$dockerUser" ] && echo "###Please pass username...!!!">>deployFileBrowser.log && exit 0 || echo "Will be deploying container for user $dockerUser">>deployFileBrowser.log;
+[ -z "$dockerUser" ] && echo "###Please pass username...!!!">>deployQbittorrent.log && exit 0 || echo "Will be deploying container for user $dockerUser">>deployQbittorrent.log;
 #installing docker-compose
 apt install docker-compose -y;
 #Deploying docker compose
-echo "Deploying File Browser...">>deployFileBrowser.log;
+echo "Deploying Qbittorrent...">>deployQbittorrent.log;
 DOCKER_USER=$dockerUser docker-compose down
 DOCKER_USER=$dockerUser docker-compose up -d
-echo "File Browser deployment successfull...">>deployFileBrowser.log;
+echo "Qbittorrent deployment successfull...">>deployQbittorrent.log;
 exit 0
