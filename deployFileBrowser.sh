3f24f0bbe428ee72f2637f413e4240ea972c5501
kameshX007 <kameshbook@gmail.com>
2022-09-04 15:09:02 +0530

    #

--- deployFileBrowser.sh
+++ deployFileBrowser.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+#Checking whether username is passed to script
+#A docker folder will be created inside this users home directory and container persistant data will be stored there
+dockerUser=$1;
+[ -z "$dockerUser" ] && echo "###Please pass username...!!!">>deployFileBrowser.log && exit 0 || echo "Will be deploying container for user $dockerUser">>deployFileBrowser.log;
+#installing docker-compose
+apt install docker-compose -y;
 #Deploying docker compose
 echo "Deploying File Browser...">>deployFileBrowser.log;
 docker-compose down
-DOCKER_USER=$1 docker-compose up -d
+DOCKER_USER=$dockerUser docker-compose up -d
 echo "File Browser deployment successfull...">>deployFileBrowser.log;
 exit 0
