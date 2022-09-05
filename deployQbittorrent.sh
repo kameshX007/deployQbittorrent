e588e5cff1042df496c6938d7162d8787383ea30
kameshX007 <112748572+kameshX007@users.noreply.github.com>
2022-09-05 16:05:50 +0530

    Update deployQbittorrent.sh

--- deployQbittorrent.sh
+++ deployQbittorrent.sh
@@ -3,8 +3,7 @@
 #A docker folder will be created inside this users home directory and container persistant data will be stored there
 dockerUser=$1;
 [ -z "$dockerUser" ] && echo "###Please pass username...!!!">>deployQbittorrent.log && exit 0 || echo "Will be deploying container for user $dockerUser">>deployQbittorrent.log;
-#installing docker-compose
-apt install docker-compose -y;
+
 #Deploying docker compose
 echo "Deploying Qbittorrent...">>deployQbittorrent.log;
 DOCKER_USER=$dockerUser docker-compose down
